7d5da15acf4bb1f262d6e4d817ea626faab676cd
johnnuez <50214042+johnnuez@users.noreply.github.com>
2022-05-24 18:56:41 +0200

    Makes external links open in new tabs (#19)

--- components/Button.js
+++ components/Button.js
@@ -1,16 +1,27 @@
 import Link from 'next/link'
 
-export default function Button({ href, size = 'lg', text }) {
+export default function Button({ href, size = 'lg', text, externalLink = false }) {
   return (
     <div className='flex items-center justify-center'>
       <div className='text-center'>
-        <Link href={`${href}`}>
+        {externalLink ? (
           <a
+            href={href}
+            target='_blank'
+            rel='noopener noreferrer'
             className={`hover:text-neutral-50 block transition-all ease-in-out duration-300 px-8 py-3 text-${size} tracking-widest sm:tracking-[0.2em] text-neutral-300 bg-gray-900 border-2 border-yellow-600 bg-opacity-0 hover:shadow-yellow-600 hover:shadow-[0_0_2px_2px_rgba(0,0,0,0.10)] hover:bg-opacity-70 border-opacity-50`}
           >
             {text.toUpperCase()}
           </a>
-        </Link>
+        ) : (
+          <Link href={`${href}`}>
+            <a
+              className={`hover:text-neutral-50 block transition-all ease-in-out duration-300 px-8 py-3 text-${size} tracking-widest sm:tracking-[0.2em] text-neutral-300 bg-gray-900 border-2 border-yellow-600 bg-opacity-0 hover:shadow-yellow-600 hover:shadow-[0_0_2px_2px_rgba(0,0,0,0.10)] hover:bg-opacity-70 border-opacity-50`}
+            >
+              {text.toUpperCase()}
+            </a>
+          </Link>
+        )}
       </div>
     </div>
   )
